cba974d4bda1e2eda6d590668a3958424b92ac09
pinglu85 <luping8576@hotmail.com>
2022-08-17 22:41:51 +0200

    add explanation

--- problem-solving-intermediate/hotel-construction/solution.js
+++ problem-solving-intermediate/hotel-construction/solution.js
@@ -3,6 +3,15 @@ function numberOfWays(roads) {
 
   let ans = 0;
 
+  // Loop through all the nodes in the graph. For each node that has more
+  // than three child nodes, start a level by level traversal.
+  // At each level, calculate the number of combinations of triplets that
+  // the current level can form. To handle the case where a child node have
+  // more than one child node, group the child nodes sharing the same parent.
+  // In other words, use a 2D array `currLevel` to hold the current level to
+  // explore, where each subarray contains the nodes that have the same parent.
+  // The traversal stops when the length of the `currLevel` is less than `3`.
+  // The `demonstration.png` illustrates the above process.
   for (const [from, destinations] of adjacencyList.entries()) {
     const seen = new Set();
     seen.add(from);
